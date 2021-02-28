a6e680afb0244087e25f4c3cb76eb6a6870d68aa
CAcerini <acerinic@gmail.com>
2021-02-28 15:57:31 +0000

    logistic regs first look

--- D2-logistic reg.R
+++ D2-logistic reg.R
@@ -24,3 +24,34 @@ ggplot(Ndata2, aes(x=Strongyles, y=survivedfirstwinter)) +
   geom_point(shape=1, position=position_jitter(width=.05,height=.05)) + 
   stat_smooth(method="glm", method.args=list(family="binomial"), se=FALSE)
 
+
+
+model2 <- glm(survivedfirstwinter ~ MassPerPellet, family = binomial, data=Ndata2)
+summary(model2)
+
+
+range(Ndata2$MassPerPellet)
+
+
+xMass <-seq (0, 2.5, 0.001)
+
+YMass <- predict(model2, list(MassPerPellet=xMass),type="response")
+plot(survivedfirstwinter~MassPerPellet, data=Ndata2, col="red4")
+lines(xMass, YMass, col = "red", lwd = 2)
+
+
+
+model3 <- glm(survivedfirstwinter ~ Flukes, family = binomial, data=Ndata2)
+summary(model3)
+
+
+range(Ndata2$Flukes)
+
+
+xFluke <-seq (0, 134, 0.1)
+
+yFluke <- predict(model3, list(Flukes=xFluke),type="response")
+
+plot(survivedfirstwinter~Flukes, data=Ndata2, col="red4")
+lines(xFluke, yFluke, col = "red", lwd = 2)
+
